e035bf23bb64bce78ad7c2ea4bf09028f1b0644c
Michael Rosario <mjfrosario@gmail.com>
2019-02-27 17:36:42 -0500

    Start Firebase integration

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,9 @@
 
   // Creating variables to hold the number of wins, losses, and ties. They start at 0.
   var name = "Player 1";
-  var challenger = "Player 2";
+  var challenger = false; // if not computer
+  var challengerId = "";
+  var status = "";
   var wins = 0;
   var losses = 0;
   var ties = 0;
@@ -43,23 +45,93 @@ var connectedRef = database.ref(".info/connected");
   database.ref('players').on("value", function(snapshot) {
     var currentlyPlaying = 0;
     $(".challenger").empty();
+  
     snapshot.forEach(function(userSnapshot) {
+      
       var currentKey = parseInt(userSnapshot.key);
       var player = userSnapshot.val();
       if(player.currentlyPlaying){
         currentlyPlaying++;
       }
-      if(currentKey !== key && !player.currentlyPlaying){
-        $(".challenger").append(`<div class="playersList Item${currentKey}">${player.name} is now available! <a href="#" data='${currentKey}'>Play Now</a></div>`);
-      } else if(currentKey !== key && player.currentlyPlaying) {
-        $(".challenger").append(`<div class="playersList Item${currentKey}">${player.name} is currently playing...`);
+
+      // currentKey != key        : Check if it's not you
+      // player.status == key     : Check if the player wants to play with you
+      // status == currentKey  : Check if you approved to play with player
+
+      if(currentKey !== key && player.status == key && status == currentKey){
+        
+        $(".challenger").html(`<div class="playerChallenge Item${currentKey}">You are now playing with ${player.name}.</div>`);
+
+        // set specifics to player
+        $(".player2").text(`${player.name}`);
+        challenger = player.name;
+        challengerId = currentKey;
+        round = 1;
+
+        database.ref("/players/"+key).update({
+          currentlyPlaying : true
+        });
+        database.ref("/players/"+currentKey).update({
+          currentlyPlaying : true
+        });
+
+      } else if(!challenger){ 
+
+        if(currentKey !== key && player.status == key){
+        $(".challenger").html(`<div class="playerChallenge Item${currentKey}">${player.name} wants to play with you! &nbsp; <a href="#" data-username='${player.name}' data-player='${currentKey}'>Accept</a></div>`);
+        } else if(currentKey !== key && !player.currentlyPlaying){
+          $(".challenger").append(`<div class="playersList Item${currentKey}">${player.name} is now available! &nbsp; <a href="#" data-username='${player.name}' data-player='${currentKey}'>Play Now</a></div>`);
+        } else if(currentKey !== key && player.currentlyPlaying) {
+          $(".challenger").append(`<div class="playersList Item${currentKey}">${player.name} is currently playing...`);
+        } else if((snapshot.numChildren() - currentlyPlaying) < 2 &&  $(".challenger").find("div").length === 0) {
+            $(".challenger").html(`Waiting for Challengers...`);
+        }
       }
+
+    });
+  });
+
+  // Send a request to play
+  $(document).on("click",".playersList a",function(e){
+    e.preventDefault();
+    
+    var challengerId = $(this).attr('data-player');
+    var challengerName = $(this).attr('data-username');
+    
+    database.ref("/players/"+key).update({
+      status : challengerId,
+      wins: 0,
+      losses: 0,
+      ties: 0,
     });
 
-    if ((snapshot.numChildren() - currentlyPlaying) < 2) {
-      $(".challenger").html(`Waiting for Challengers...`);
-    }
+    $(".Item"+challengerId).html(`Asking <strong>${challengerName}</strong> to play... Please wait...`);
+
+  });
+
+  // Accept request to player
+  $(document).on("click",".playerChallenge a",function(e){
+    e.preventDefault();
     
+    var challengerId = $(this).attr('data-player');
+    var challengerName = $(this).attr('data-username');
+    
+    // set player
+    database.ref("/players/"+key).update({
+      status : challengerId,
+      wins: 0,
+      losses: 0,
+      ties: 0,
+    });
+    
+    challenger = challengerName;
+    challengerId = challengerId;
+    round = 1;
+    
+    $(".player2").text(challenger);
+
+    $(".Item"+challengerId).html(`You are now playing with <strong>${challengerName}</strong>.`);
+
   });
 
   // Check if data removed
@@ -68,9 +140,26 @@ var connectedRef = database.ref(".info/connected");
     $(".Item"+data.key).remove();
   });
 
+  var userChoice = "";
+  var challengerChoice = "";
+  
   // Check if data changed
   database.ref('players').on('child_changed', function(data) {
     console.log(data.val().name+" played the game...");
+    if(challenger && data.key == key){
+      userChoice = data.val().currentChoice;
+      $(".status").html(`Waiting for ${challenger}...`);
+
+    }
+    if(challenger && data.key == challengerId){
+      challengerChoice = data.val().currentChoice;
+      $(".status").html(`Waiting for ${name}...`);
+    }
+    if(challenger && userChoice && challengerChoice){
+      $(".status").html(`Round ${round++}`);
+      runMultiplayerGame(userChoice,challengerChoice);
+
+    }
   });
 
   $(".start").on("click",function(e){
@@ -98,7 +187,8 @@ var connectedRef = database.ref(".info/connected");
       ready = true;
 
       database.ref("/players/"+key).set({
-        name: name
+        name: name,
+        status
       });
       
     }
@@ -111,6 +201,7 @@ var connectedRef = database.ref(".info/connected");
         wins = parseInt(snapshot.val().wins) || 0;
         ties = parseInt(snapshot.val().ties) || 0;
         losses = parseInt(snapshot.val().losses) || 0;
+        status = parseInt(snapshot.val().status) || 0;
     
         $("#wins").html(`<span>wins:</span> ${wins}`);
         $("#losses").html(`<span>losses:</span> ${losses}`);
@@ -131,23 +222,57 @@ var connectedRef = database.ref(".info/connected");
         return; // exclude on key up on input
       }  
 
-      runGame(event.key);
+      if(ready && !challenger){
+        runGame(event.key);
+      } else if(ready){
+        updateMultiplayerGame(event.key);
+      }
         
     }
 
     $(".button").on("mouseup touchstart",function(){
-
-      if(ready){
-        runGame($(this).attr("data-option"));
-      } else {
-
+      var choice = $(this).attr("data-option");
+      if(ready && !challenger){
+        runGame(choice);
+      } else if(ready){
+        updateMultiplayerGame(choice);
       }
       return false;
 
     });
 
+    function updateMultiplayerGame(input){
+
+       // Determines which key was pressed.
+       var userGuess = input.toLowerCase();
+       
+       if(userGuess === "r"){
+          $("#rock").addClass("active");
+        }
+        if(userGuess === "p"){
+          $("#paper").addClass("active");
+        }
+        if(userGuess === "s"){
+          $("#scissors").addClass("active");
+        }
+
+        database.ref("/players/"+key).update({
+          currentChoice: userGuess
+        });
+
+    }
+
+    function runMultiplayerGame(userInput,challengerInput){
+      console.log("userInput",userInput);
+      console.log("challengerInput",challengerInput);
+      if(ready){
+        ready = false;
+      }
+
+    }
+
     function runGame(input) {
-        
+
         // Determines which key was pressed.
         var userGuess = input.toLowerCase();
 
